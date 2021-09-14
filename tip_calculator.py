55549c0d572b702ce9e5e6a1a0c3bbbf0e106ee6
Júlio Billet <76001545+juliobillet@users.noreply.github.com>
2021-09-14 15:35:04 -0300

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,4 +1,4 @@
-print("Welcome to the tip calculator.")
+print("Welcome to the tip calculator!")
 total_bill = float(input("What was the total bill? $"))
 
 tip_percentage = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
